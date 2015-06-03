467f4244708d690942b17751bf5182e8199c04d1
Kyle Richardson <s.k.richardson7@gmail.com>
2015-06-03 16:52:40 -0400

    Not linked up properly.

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -3,8 +3,16 @@ this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1
     return "<h1>Hello</h1>\n";
 },"useData":true});
 this["JST"]["chat"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "";
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+
+  return "<li>\n\n  "
+    + alias3(((helper = (helper = helpers.username || (depth0 != null ? depth0.username : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"username","hash":{},"data":data}) : helper)))
+    + "\n  "
+    + alias3(((helper = (helper = helpers.created_at || (depth0 != null ? depth0.created_at : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"created_at","hash":{},"data":data}) : helper)))
+    + "\n  "
+    + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
+    + "\n\n</li>\n";
 },"useData":true});
 this["JST"]["login"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<form class=\"login-form\">\n  <input type=\"text\" placeholder=\"Username\">\n <input type=\"submit\" value=\"Log In\">\n</form>\n";
+    return "<form class=\"login-form\">\n  <input class =\"login-form-username\" type=\"text\" placeholder=\"Username\">\n <input type=\"submit\" value=\"Log In\">\n</form>\n";
 },"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,62 @@
 (function(){
   'use strict';
 
+  //http://tiny-lasagna-server.herokuapp.com/
+
+  var username = '';
+
+
+
   $(document).ready(function(){
-    $('.login-form').on('submit', function(event){
-      event.prevetDefault();
+
+    route();
+
+    $(document).on('submit', '.login-form', function(event){
+      event.preventDefault();
+      username = ($(this).find('.login-form-username').val());
       window.location.hash = '/chat';
     });
 
     $(window).on('hashchange',function(event){
       event.preventDefault();
-      console.log(sindow.location.hash);
+      route();
 
     });
   });
+
+  function route(){
+    switch(window.location.hash){
+    case'':
+      $('.login').html(JST['login']());
+      break;
+    case '#/chat':
+      renderChat();
+      break;
+    }
+  }
+
+
+function renderChat(){
+  $('.application').html(JST['chat']());
+    console.log('username:', username);
+    $.ajax({
+     url:  "http://tiny-lasagna-server.herokuapp.com/collections/messages",
+  }).then(function(messages){
+    $('.chat').html(JST['chat'](messages));
+    console.log(messages);
+  });
+}
+//    $.ajax({
+//    url:"http://tiny-lasagna-server.herokuapp.com/collections/messages/",
+//    type: "POST",
+//    data: {
+//      username: "kyle",
+//      created_at: new Date(),
+//      content: "public failure."
+//    }
+//
+//
+// });
+
+
 })();
